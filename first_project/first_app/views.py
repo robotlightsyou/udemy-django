84c8ecc9257c001bbf4f8d089ccdb8a1d7e6a6e0
Adam Earle <robotlightsyou@gmail.com>
2021-02-14 17:59:10 -0800

    add static files

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -8,3 +8,6 @@ def index(request):
     my_dict = {'insert_me': "Hello, I am from views.py.", 'second': 'I am the second piece of context.'}
     return render(request, "first_app/index.html", context=my_dict)
 
+def help(request):
+    temps = {'help': 'Help Page'}
+    return render(request, 'first_app/help.html', context=temps)
