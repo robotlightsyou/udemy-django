1cdff9534ebefc47ef35ccb056de66debfdca8f5
Adam Earle <robotlightsyou@gmail.com>
2021-02-15 21:18:12 -0800

    section  2 prefinal

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-
-# Create your views here.
+from .models import Topic, Webpage, AccessRecord
 
 
 def index(request):
-    my_dict = {'insert_me': "Hello, I am from views.py.", 'second': 'I am the second piece of context.'}
-    return render(request, "first_app/index.html", context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+    return render(request, "first_app/index.html", context=date_dict)
 
 def help(request):
     temps = {'help': 'Help Page'}
