4020a7653120d2237b33a0ab5dc21b3b166d89ec
Adam Earle <robotlightsyou@gmail.com>
2021-02-16 16:06:35 -0800

    forms p2

--- third/basicapp/urls.py
+++ third/basicapp/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from basicapp import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('basicapp.urls')),
+    path("", views.index, name='index'),
+    path("formpage", views.form_name_view, name='form_name'),
 ]
