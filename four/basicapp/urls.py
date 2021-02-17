724405fbfc24587bd0a03766779b234f31b424a2
Adam Earle <robotlightsyou@gmail.com>
2021-02-17 14:06:20 -0800

    html relative links started

--- four/basicapp/urls.py
+++ four/basicapp/urls.py
@@ -18,4 +18,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('other/', views.other, name='other'),
+    path('relative/', views.relative, name='relative'),
 ]
