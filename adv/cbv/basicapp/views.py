f6e59d9edec3977bd70dca9f4598d29a9617eb2a
robotlightsyou <robotlightsyou@gmail.com>
2021-03-01 12:22:40 -0800

    basic CBVs

--- adv/cbv/basicapp/views.py
+++ adv/cbv/basicapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View
 from django.http import HttpResponse
 
+
+class CBView(View):
+    def  get(self, request):
+       return HttpResponse("Class Based Views are cool!") 
+
 # Create your views here.
-def index(request):
-    return HttpResponse("hello world")
+# def index(request):
+#     return render(request, 'basicapp/index.html') 
