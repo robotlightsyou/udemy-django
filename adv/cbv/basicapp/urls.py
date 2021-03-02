7dd07d903318952f6c5736b3ee45e011ec69101b
robotlightsyou <robotlightsyou@gmail.com>
2021-03-02 10:16:40 -0800

    CBV TemplateView

--- adv/cbv/basicapp/urls.py
+++ adv/cbv/basicapp/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CBView.as_view()),
+    path('', views.IndexView.as_view()),
 ]
--- adv/cbv/basicapp/views.py
+++ adv/cbv/basicapp/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
+from django.views.generic import View, TemplateView
 
 
-class CBView(View):
-    def  get(self, request):
-       return HttpResponse("Class Based Views are cool!") 
+class IndexView(TemplateView):
+    template_name = 'basicapp/index.html'
 
-# Create your views here.
-# def index(request):
-#     return render(request, 'basicapp/index.html') 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'Basic Injection'
+        return context
