13215d03e7a7538135c1cfa6ed446b142901d889
robotlightsyou <robotlightsyou@gmail.com>
2021-03-02 14:17:19 -0800

    CBVs done

--- adv/cbv/basicapp/urls.py
+++ adv/cbv/basicapp/urls.py
@@ -20,4 +20,8 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('list', views.SchoolListView.as_view(), name='list'),
     path('<int:pk>/', views.SchoolDetailView.as_view(), name='detail'),
+    path('update/<int:pk>/', views.SchoolUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', views.SchoolDeleteView.as_view(), name='delete'),
+    path('create/', views.SchoolCreateView.as_view(), name='create'),
+    # path('update/<int:pk>', views.SchoolUpdateView.as_view(), name='update'),
 ]
