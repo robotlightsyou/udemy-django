1c43ebb5761325a3515369342c0f3d2ee2dea31d
Adam Earle <robotlightsyou@gmail.com>
2021-04-03 20:36:11 -0700

    Blog (#13)
    * oops section one
    
    * first models
    
    * section  2 prefinal
    
    * start users project
    
    * section two complete
    
    * oops to main
    
    * minor cleanup
    
    * second pro final touches
    
    * start section 3 - forms
    
    * forms p2
    
    * forms p3
    
    * section 3 finished
    
    * section 4 setup
    
    * html relative links started
    
    * section 4 finished
    
    * section 5 setup
    
    * add settings
    
    * project restructure
    
    * profile models added
    
    * profile forms
    
    * registration
    
    * section 99% done
    
    having issues saving profile pics from the form. Posted to the forum, waiting for an update on how to successfully execute.
    
    * update media folder
    
    * add default picture
    
    * adv setup
    
    * lvl1 troubleshoots
    
    * image still not working
    
    * delete
    
    * finsih level 5
    
    * i suck at git
    
    * basic CBVs
    
    * CBV TemplateView
    
    * list/detail views
    
    * CBVs done
    
    * blog initial setup
    
    * blog initial setup
    
    * Delete settings.py
    
    * trying to delete
    
    * blog initial setup
    
    * blog no color change

---
+++ adv/blogclone/core/blog/urls.py
@@ -0,0 +1,31 @@
+"""blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from blog import views
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='post_list'), 
+    path('post/drafts', views.DraftListView.as_view(), name='post_draft_list'), 
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+]
---
+++ adv/blogclone/core/blog/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.urls import reverse_lazy
+from .models import Post, Comment
+from .forms import Post_Form, Comment_Form
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (TemplateView, ListView,
+                                    DetailView, CreateView,
+                                    UpdateView, DeleteView,)
+
+
+def index(request):
+    return render(request, "blog/index.html")
+
+class AboutView(TemplateView):
+    template_name = 'blog/about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
+        
+class PostDetail(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = Post_Form
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = Post_Form
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = 'login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(pub_date__isnull=True).order_by('-create_date')
+    
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = Comment_Form(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = Comment_Form
+    return render(request, 'blog/comment_form.html', {'form':form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+    
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
